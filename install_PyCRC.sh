9add26088f278ad57099c284fdfe4f4436d2ab7a
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2020-03-25 16:44:57 +0900

    fix: PyCRC URL

--- install_PyCRC.sh
+++ install_PyCRC.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-wget https://pypi.python.org/packages/source/P/PyCRC/PyCRC-1.21.tar.gz
+wget http://qpypi3.qpython.org/repository/1745924/PyCRC-1.21.tar.gz
 tar -xzvf PyCRC-1.21.tar.gz
 cd PyCRC-1.21
 sudo /usr/bin/python2 setup.py install
